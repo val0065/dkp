304c1590deaa6699e7656323cce692b6a5eed460
david <woozie28@gmail.com>
2020-08-19 15:06:39 +0200

    make it better

--- server.js
+++ server.js
@@ -61,10 +61,15 @@ app.post('/upload', (req, res) => {
 
 // Static if in PROD
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('../client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+        app.get('*', function(req, res) {
+          res.sendFile('index.html', { root: __dirname }, function(err) {
+            if (err) {
+              res.status(500).send(err);
+            }
+          });
+        });
 }
 
 const port = process.env.PORT || 5000;
