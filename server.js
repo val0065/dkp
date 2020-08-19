445df7ec8c1e9bc08cc788c2561ec65bfc70e4a7
david <woozie28@gmail.com>
2020-08-19 15:18:35 +0200

    make it better

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/upload', (req, res) => {
 
 // Static if in PROD
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+    app.use('/static', express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
