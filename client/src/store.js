ae7f01ab0250c9fb51212042b1cba60c7162d99b
david <woozie28@gmail.com>
2020-08-19 15:42:19 +0200

    make it better

--- client/src/store.js
+++ client/src/store.js
@@ -5,8 +5,7 @@ import rootReducer from './reducers';
 const initialState = {};
 const middleWare = [thunk];
 const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleWare)
 ));
 
 export default store;
